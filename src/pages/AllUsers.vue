84bdee81b91caf76ab0ee5e11e557aebb1c30512
Di Qian <diqian@gmail.com>
2023-10-18 22:06:11 -0400

    animationvue

---
+++ src/pages/AllUsers.vue
@@ -0,0 +1,3 @@
+<template>
+ <div class="container"><h2>All Users</h2><router-link to="/goals">Course Goals</router-link></div>
+</template>
---
+++ src/pages/CourseGoals.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container"><h2>Course Goals</h2><router-link to="/">All Users</router-link></div>
+</template>
